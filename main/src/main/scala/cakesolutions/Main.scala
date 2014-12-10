5e593072a7cfdfec03235fc9586ea6c56442cffa
Carl Pulley <carlp@cakesolutions.net>
2014-12-10 13:11:25 +0000

    Comment tidy ups

--- main/src/main/scala/cakesolutions/Main.scala
+++ main/src/main/scala/cakesolutions/Main.scala
@@ -1,6 +1,6 @@
 package cakesolutions
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorRef, ActorSystem}
 import akka.contrib.pattern.ClusterSharding
 import cakesolutions.api.WithApi
 import cakesolutions.etcd.WithEtcd
@@ -8,22 +8,21 @@ import scala.concurrent.ExecutionContext
 
 class Main extends BootableCluster(ActorSystem("HelloWorld")) with MinNumJoinConstraint with Configuration with WithEtcd with WithApi {
 
-  def boot = new BootedNode with api.Service {
+  def boot(handler: ActorRef) = new BootedNode with api.Service {
     override def api = Some((ec: ExecutionContext) => {
-      // As shard regions may be subjected to re-balancing, we dynamically lookup the location of a shard region
-      applicationRoute(ClusterSharding(system).shardRegion(HelloWorld.shardName))(ec)
+      applicationRoute(handler)(ec)
     })
   }
 
   cluster.registerOnMemberUp {
     // Register and boot the microservice when member is 'Up'
-    ClusterSharding(system).start(
+    val actorRef = ClusterSharding(system).start(
       typeName = HelloWorld.shardName,
       entryProps = HelloWorld.shardProps,
       idExtractor = HelloWorld.idExtractor,
       shardResolver = HelloWorld.shardResolver
     )
-    boot.api.foreach(startupApi)
+    boot(actorRef).api.foreach(startupApi)
   }
 
 }
--- main/src/main/scala/cakesolutions/api/Service.scala
+++ main/src/main/scala/cakesolutions/api/Service.scala
@@ -15,8 +15,7 @@ trait Service extends Directives with Configuration with ExceptionLogging {
 
   implicit val timeout: Timeout = Timeout(config.getDuration("application.timeout", SECONDS).seconds)
 
-  // actorRef here may be a shard region, so we need to lazily evaluate this actor reference for each message that's routed
-  def applicationRoute(actorRef: => ActorRef)(implicit ec: ExecutionContext) = {
+  def applicationRoute(actorRef: ActorRef)(implicit ec: ExecutionContext) = {
     path("ping") {
       get {
         complete {

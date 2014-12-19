b266b477beb68b3d4bf68e3ebb162563a3febb34
Carl Pulley <carlp@cakesolutions.net>
2014-12-19 14:35:59 +0000

    Added license and applied guava fix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,11 +10,11 @@ object Dependencies {
 
     val actor                 = "com.typesafe.akka"   %% "akka-actor"                    % version
     val cluster               = "com.typesafe.akka"   %% "akka-cluster"                  % version
-    val contrib               = "com.typesafe.akka"   %% "akka-contrib"                  % version
+    val contrib               = "com.typesafe.akka"   %% "akka-contrib"                  % version intransitive()
     val kernel                = "com.typesafe.akka"   %% "akka-kernel"                   % version
     val multi_node_testkit    = "com.typesafe.akka"   %% "akka-multi-node-testkit"       % version
-    val persistence           = "com.typesafe.akka"   %% "akka-persistence-experimental" % version
-    val persistence_cassandra = "com.github.krasserm" %% "akka-persistence-cassandra"    % "0.4-SNAPSHOT"
+    val persistence           = "com.typesafe.akka"   %% "akka-persistence-experimental" % version intransitive()
+    val persistence_cassandra = "com.github.krasserm" %% "akka-persistence-cassandra"    % "0.3.4" intransitive()
     val remote                = "com.typesafe.akka"   %% "akka-remote"                   % version
     val slf4j                 = "com.typesafe.akka"   %% "akka-slf4j"                    % version
     val testkit               = "com.typesafe.akka"   %% "akka-testkit"                  % version
@@ -35,13 +35,14 @@ object Dependencies {
     val core = "org.scalaz" %% "scalaz-core"  % version
   }
 
-  val commons       = "org.apache.commons" %  "commons-lang3"   % "3.3.2"
-  val etcd          = "net.nikore.etcd"    %% "scala-etcd"      % "0.7"
-  val logback       = "ch.qos.logback"     %  "logback-classic" % "1.1.2"
-  val scalacheck    = "org.scalacheck"     %% "scalacheck"      % "1.11.6"
-  val scalacompiler = "org.scala-lang"     % "scala-compiler"   % scala
-  val scalatest     = "org.scalatest"      %% "scalatest"       % "2.2.1"
-  val specs2        = "org.specs2"         %% "specs2"          % "3.0-M1"
-  val typesafe      = "com.typesafe"       %  "config"          % "1.2.1"
+  val cassandra_driver = "com.datastax.cassandra" %  "cassandra-driver-core" % "2.1.3"
+  val commons          = "org.apache.commons"     %  "commons-lang3"         % "3.3.2"
+  val etcd             = "net.nikore.etcd"        %% "scala-etcd"            % "0.7"
+  val logback          = "ch.qos.logback"         %  "logback-classic"       % "1.1.2"
+  val scalacheck       = "org.scalacheck"         %% "scalacheck"            % "1.11.6"
+  val scalacompiler    = "org.scala-lang"         %  "scala-compiler"        % scala
+  val scalatest        = "org.scalatest"          %% "scalatest"             % "2.2.1"
+  val specs2           = "org.specs2"             %% "specs2"                % "3.0-M1"
+  val typesafe         = "com.typesafe"           %  "config"                % "1.2.1"
 
 }
